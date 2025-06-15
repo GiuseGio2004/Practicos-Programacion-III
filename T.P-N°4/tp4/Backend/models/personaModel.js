598fe05aa3fbe2b828b55e4ffd4716a0a7ba3ef2
Giuseppe Giovanelli <giusegio2004@gmail.com>
2025-06-15 00:38:25 -0300

    Add files via upload

---
+++ T.P-N°4/tp4/Backend/models/personaModel.js
@@ -0,0 +1,9 @@
+const personas = [
+  { id: 1, nombre: "Lucas", apellido: "Gomez", edad: 28, email: "lucas@gmail.com" },
+  { id: 2, nombre: "Juan", apellido: "Martinez", edad: 35, email: "Juan@yahoo.com" },
+  { id: 3, nombre: "Carlos", apellido: "Perez", edad: 42, email: "carlosp@hotmail.com" },
+  { id: 4, nombre: "Sara", apellido: "Lopez", edad: 22, email: "sara@gmail.com" },
+  { id: 5, nombre: "Maia", apellido: "Fernandez", edad: 30, email: "maia@mail.com" }
+];
+
+module.exports = personas;
